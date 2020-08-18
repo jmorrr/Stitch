defef81d38562bf58554795350740f9cda99fdd2
jmorrr <57759315+jmorrr@users.noreply.github.com>
2020-08-18 18:04:58 +0300

    Update stitch.py
    Passing a path to imageJ and then using a space " " to split the arguments wasn't effective if someone was using a filepath with a space in the directory name. Hence the update here and in the imageJ macro where the split now uses "<>" to separate.

--- Stitch_package_tool/src/stitch.py
+++ Stitch_package_tool/src/stitch.py
@@ -419,7 +419,7 @@ def ij_macro_processor(fus_tiffs_dict, py_file_loc):
         else:
             logging.info('Running imageJ macro on ' + key)
             IJ.runMacroFile(stitch_path + r"\stitch_macro.ijm",
-                            key + " " + fus_tiffs_dict[key][0])
+                            key + "<>" + fus_tiffs_dict[key][0])
             IJ.saveAs("tiff", fus_tiffs_dict[key][1] + '/'
                       + os.path.basename(key))
             IJ.run("Close All")
